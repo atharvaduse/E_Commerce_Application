2a243646bded9036fbf86e55339633f77339899e
atharvaduse <atharvaduse@gmail.com>
2024-12-28 15:58:18 +0530

    Sorting of Data

--- sb-ecom/src/main/java/com/ecommorce/project/service/CategoryService.java
+++ sb-ecom/src/main/java/com/ecommorce/project/service/CategoryService.java
@@ -6,7 +6,7 @@ import com.ecommorce.project.payload.CategoryResponse;
 
 public interface CategoryService {
 
-    CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize,String sortBy,String sortOrder);
 
     CategoryDTO createCategory(CategoryDTO categoryDTO);
 
